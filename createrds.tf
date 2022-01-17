520e4535c33f79ea8e1ac14e23c35c31417af150
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-17 21:27:42 +0530

    Create createrds.tf

---
+++ createrds.tf
@@ -0,0 +1,15 @@
+resource "aws_db_instance" "default" {
+  allocated_storage    = 10
+  engine               = "mysql"
+  engine_version       = "5.7"
+  instance_class       = "t2.micro"
+  name                 = "mynewdb"
+  username             = "foo"
+  password             = "foobarbaz"
+  parameter_group_name = "default.mysql5.7"
+  skip_final_snapshot  = true
+}
+resource "aws_db_instance" "example" {
+  allocated_storage     = 50
+  max_allocated_storage = 100
+}
