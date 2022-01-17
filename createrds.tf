d79c5f35005c2c042d7ed9d4fdefd2c972e45832
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-17 21:43:03 +0530

    Update createrds.tf

--- createrds.tf
+++ createrds.tf
@@ -12,7 +12,3 @@ resource "aws_db_instance" "default" {
   parameter_group_name = "default.mysql5.7"
   skip_final_snapshot  = true
 }
-resource "aws_db_instance" "example" {
-  allocated_storage     = 50
-  max_allocated_storage = 100
-}
