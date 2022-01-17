cb8c149e35c10452413eccabb4d040d6cb7d9e57
muni1430 <93640586+muni1430@users.noreply.github.com>
2022-01-17 21:38:48 +0530

    Update createrds.tf

--- createrds.tf
+++ createrds.tf
@@ -2,7 +2,7 @@ resource "aws_db_instance" "default" {
   allocated_storage    = 10
   engine               = "mysql"
   engine_version       = "5.7"
-  instance_class       = "t2.micro"
+  instance_class       = "db.t2.micro"
   name                 = "mynewdb"
   username             = "foo"
   password             = "foobarbaz"
